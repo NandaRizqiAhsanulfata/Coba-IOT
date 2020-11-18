bf7c9465941f0777361e5b36b1b132a5b00a87f5
NandaRizqiAhsanulfata <nandarizqi68@gmail.com>
2020-11-18 13:16:31 +0700

    Smart Irrigation

--- app/src/main/java/com/example/cobaiot/MainActivity.java
+++ app/src/main/java/com/example/cobaiot/MainActivity.java
@@ -17,15 +17,19 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button buttonSwitchOn, buttonSwitchOff, buttonServoUp, buttonServoDown;
+    Button buttonSwitchOn, buttonSwitchOff, buttonSwitchOn2, buttonSwitchOff2, buttonServoUp, buttonServoDown;
 
     private DatabaseReference databaseReference;
     private static final String TAG = "MainActivity";
-    private TextView dtl;
-    private TextView tv_lampu;
+    private TextView dtl_tanah;
+    private TextView dtl_suhu;
+    private TextView pompa;
+    private TextView pendingin;
 
     private Firebase mRef;
+    private Firebase mRef1;
     private Firebase mRef2;
+    private Firebase mRef3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,20 +38,40 @@ public class MainActivity extends AppCompatActivity {
 
         buttonSwitchOn = findViewById(R.id.btn_on);
         buttonSwitchOff = findViewById(R.id.btn_off);
-        buttonServoUp = findViewById(R.id.srv_up);
-        buttonServoDown = findViewById(R.id.srv_down);
+        buttonSwitchOn2 = findViewById(R.id.btn_on2);
+        buttonSwitchOff2 = findViewById(R.id.btn_off2);
+        buttonServoUp = findViewById(R.id.srv_open);
+        buttonServoDown = findViewById(R.id.srv_close);
 
-        dtl = (TextView) findViewById(R.id.dtl);
-        tv_lampu = (TextView) findViewById(R.id.tv_lampu);
+        dtl_tanah = (TextView) findViewById(R.id.dtl_tanah);
+        dtl_suhu = (TextView) findViewById(R.id.dtl_suhu);
+        pompa = (TextView) findViewById(R.id.pompa);
+        pendingin = (TextView) findViewById(R.id.pendingin);
 
-        mRef = new Firebase("https://relay-99773.firebaseio.com/Moisture");
-        mRef2 = new Firebase("https://relay-99773.firebaseio.com/Status_Led");
+        mRef = new Firebase("https://relay-99773.firebaseio.com/Kelembaban");
+        mRef1 = new Firebase("https://relay-99773.firebaseio.com/Suhu");
+        mRef2 = new Firebase("https://relay-99773.firebaseio.com/Status_Pompa");
+        mRef3 = new Firebase("https://relay-99773.firebaseio.com/Status_Pendingin");
 
         mRef.addValueEventListener(new com.firebase.client.ValueEventListener() {
             @Override
             public void onDataChange(com.firebase.client.DataSnapshot dataSnapshot) {
                 String value = dataSnapshot.getValue(String.class);
-                dtl.setText(value);
+                dtl_tanah.setText(value);
+                Log.d(TAG, "Sensor : " + value);
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
+
+        mRef1.addValueEventListener(new com.firebase.client.ValueEventListener() {
+            @Override
+            public void onDataChange(com.firebase.client.DataSnapshot dataSnapshot) {
+                String value = dataSnapshot.getValue(String.class);
+                dtl_suhu.setText(value);
                 Log.d(TAG, "Sensor : " + value);
             }
 
@@ -61,7 +85,20 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 String value = dataSnapshot.getValue(String.class);
-                    tv_lampu.setText(value);
+                    pompa.setText(value);
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
+
+        mRef3.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String value = dataSnapshot.getValue(String.class);
+                    pendingin.setText(value);
             }
 
             @Override
@@ -71,7 +108,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
         databaseReference = FirebaseDatabase.getInstance().getReference();
-        final DatabaseReference databaseReferenceLED = databaseReference.child("LED_Status");
+        final DatabaseReference databaseReferenceLED = databaseReference.child("Pompa_Air");
 
         buttonSwitchOn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -87,7 +124,23 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        final DatabaseReference databaseReferenceServo = databaseReference.child("Servo_Status");
+        final DatabaseReference databaseReferenceLED2 = databaseReference.child("Pendingin");
+
+        buttonSwitchOn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                databaseReferenceLED2.setValue(1);
+            }
+        });
+
+        buttonSwitchOff2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                databaseReferenceLED2.setValue(0);
+            }
+        });
+
+        final DatabaseReference databaseReferenceServo = databaseReference.child("Servo");
 
         buttonServoUp.setOnClickListener(new View.OnClickListener() {
             @Override
