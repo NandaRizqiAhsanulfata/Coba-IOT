542511fbe67cf42cb5db074479d1cab0e1420206
NandaRizqiAhsanulfata <nandarizqi68@gmail.com>
2020-11-21 00:52:28 +0700

    Tambah Batas Kelembaban

--- app/src/main/java/com/example/cobaiot/MainActivity.java
+++ app/src/main/java/com/example/cobaiot/MainActivity.java
@@ -5,7 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.firebase.client.DataSnapshot;
@@ -25,16 +28,51 @@ public class MainActivity extends AppCompatActivity {
     private TextView dtl_suhu;
     private TextView pompa;
     private TextView pendingin;
+    private TextView batasatas;
 
     private Firebase mRef;
     private Firebase mRef1;
     private Firebase mRef2;
     private Firebase mRef3;
+    private Firebase mRef4;
+
+    private Spinner Batas_Atas;
+    private String[] batasAtas = {
+            "Basah",
+            "Sedang"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Batas_Atas = (Spinner) findViewById(R.id.spinner);
+        // inisialiasi Array Adapter dengan memasukkan string array di atas
+        final ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_item, batasAtas);
+        // mengeset Array Adapter tersebut ke Spinner
+        Batas_Atas.setAdapter(adapter);
+        // mengeset listener untuk mengetahui saat item dipilih
+        Batas_Atas.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                // Aksi yang dilakukan ketika memilih item (diambil dari adapter)
+                final DatabaseReference databaseReferenceServo = databaseReference.child("Batas_Atas");
+                String m = adapter.getItem(i);
+                if (m == "Basah"){
+                    databaseReferenceServo.setValue("Basah");
+                }
+                else {
+                    databaseReferenceServo.setValue("Sedang");
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
 
         buttonSwitchOn = findViewById(R.id.btn_on);
         buttonSwitchOff = findViewById(R.id.btn_off);
@@ -47,11 +85,13 @@ public class MainActivity extends AppCompatActivity {
         dtl_suhu = (TextView) findViewById(R.id.dtl_suhu);
         pompa = (TextView) findViewById(R.id.pompa);
         pendingin = (TextView) findViewById(R.id.pendingin);
+        batasatas = (TextView) findViewById(R.id.bts_ats);
 
         mRef = new Firebase("https://relay-99773.firebaseio.com/Kelembaban");
         mRef1 = new Firebase("https://relay-99773.firebaseio.com/Suhu");
         mRef2 = new Firebase("https://relay-99773.firebaseio.com/Status_Pompa");
         mRef3 = new Firebase("https://relay-99773.firebaseio.com/Status_Pendingin");
+        mRef4 = new Firebase("https://relay-99773.firebaseio.com/Batas_Atas");
 
         mRef.addValueEventListener(new com.firebase.client.ValueEventListener() {
             @Override
@@ -99,6 +139,18 @@ public class MainActivity extends AppCompatActivity {
             public void onDataChange(DataSnapshot dataSnapshot) {
                 String value = dataSnapshot.getValue(String.class);
                     pendingin.setText(value);
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
+        mRef4.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String value = dataSnapshot.getValue(String.class);
+                batasatas.setText(value);
             }
 
             @Override
